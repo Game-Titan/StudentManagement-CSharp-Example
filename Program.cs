e1a867ef6f21053ca684e4ddd79c9c301bbfcc1b
Game-Titan <tinypool567@gmail.com>
2025-06-20 18:45:07 +0500

    Initial commit: Basic C# Student Class
    This app demonstrates classes, objects, and console I/O.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Data;
+using System.Runtime.CompilerServices;
+using System.Threading.Channels;
+
+namespace CODEMONKEY_LEARNING
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+         
+            Student student1 = new Student("Alice Smith", 1001, "ComputerScience");
+            Student student2 = new Student("Bob Johnson", 1002, "Electrical Engineering");
+            student1.DisplayStudentInfo();
+            student2.DisplayStudentInfo();
+
+
+        }
+        class Student
+        {
+            string name;
+            int ID;
+            string major;
+            public Student(string name, int ID, string major)
+            {
+                this.name = name;
+                this.ID = ID;
+                this.major = major;
+            }
+
+            public void DisplayStudentInfo()
+            {
+                Console.WriteLine("  Student Information:");
+                Console.WriteLine($"  Name: {name}");
+                Console.WriteLine($"  Student ID: {ID}");
+                Console.WriteLine($"  Major: {major}");
+                Console.WriteLine();
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
